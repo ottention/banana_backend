cce4ef339ec4ea80336ac9ec66b5c9556e847e9e
CMHyuk <alsgur9710@naver.com>
2023-05-09 15:39:21 +0900

    feat : BusinessCardTag entity

---
+++ src/main/java/com/ottention/banana/entity/BusinessCardTag.java
@@ -0,0 +1,37 @@
+package com.ottention.banana.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public class BusinessCardTag {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "businessCardTag_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @ManyToOne
+    @JoinColumn(name = "card_id")
+    private BusinessCard businessCard;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
