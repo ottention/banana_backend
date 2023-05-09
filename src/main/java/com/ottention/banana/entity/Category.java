da8d00abb9723a5b96a5b4aab971da9a10dd5d03
CMHyuk <alsgur9710@naver.com>
2023-05-09 15:37:50 +0900

    feat : Category entity

---
+++ src/main/java/com/ottention/banana/entity/Category.java
@@ -0,0 +1,36 @@
+package com.ottention.banana.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Category {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    private String categoryName;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
