bfbe8fb7a91a59e6c157600e245555e39ee45641
CMHyuk <alsgur9710@naver.com>
2023-05-09 15:38:40 +0900

    feat : User entity

---
+++ src/main/java/com/ottention/banana/entity/User.java
@@ -0,0 +1,32 @@
+package com.ottention.banana.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter @Setter
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String nickName;
+    private String email;
+
+    @Builder
+    public User(String nickName, String email) {
+        this.nickName = nickName;
+        this.email = email;
+    }
+}
