3fcbed227c6595edca0a3ef5c142d18d2eb8ed53
CMHyuk <alsgur9710@naver.com>
2023-05-09 15:38:33 +0900

    feat : Tag entity

---
+++ src/main/java/com/ottention/banana/entity/Tag.java
@@ -0,0 +1,31 @@
+package com.ottention.banana.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Tag {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String name; //태그명
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
