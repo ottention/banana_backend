a3240bdd3433d10e71c715df2e948ea77dd4c905
CMHyuk <alsgur9710@naver.com>
2023-05-09 15:38:08 +0900

    feat : Image entity

---
+++ src/main/java/com/ottention/banana/entity/Image.java
@@ -0,0 +1,36 @@
+package com.ottention.banana.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Image {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    private String imageUrl; //이미지 주소
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "card_id")
+    private BusinessCard businessCard;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
