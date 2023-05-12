c9c9991572894284ed033a573a7c1cfdf897d341
CMHyuk <alsgur9710@naver.com>
2023-05-12 22:45:00 +0900

    chore : Delete unused import statement

--- src/main/java/com/ottention/banana/entity/QRCode.java
+++ src/main/java/com/ottention/banana/entity/QRCode.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
