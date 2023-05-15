9ff9405c9c17198e4b9eb1cee238c18cb776b194
CMHyuk <alsgur9710@naver.com>
2023-05-15 12:15:09 +0900

    feat : Add EnableConfigurationProperties

--- src/main/java/com/ottention/banana/BananaApplication.java
+++ src/main/java/com/ottention/banana/BananaApplication.java
@@ -1,11 +1,16 @@
 package com.ottention.banana;
 
+import com.ottention.banana.config.GoogleConfig;
+import com.ottention.banana.config.JwtConfig;
+import com.ottention.banana.config.KakaoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties({JwtConfig.class, KakaoConfig.class})
 public class BananaApplication {
 
 	public static void main(String[] args) {
