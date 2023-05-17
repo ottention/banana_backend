7899854fbe5eae1c6ee66aa75a747f312a9226cd
CMHyuk <alsgur9710@naver.com>
2023-05-17 23:19:53 +0900

    feat : ErrorResponse class

---
+++ src/main/java/com/ottention/banana/response/error/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.ottention.banana.response.error;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    @Builder
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
