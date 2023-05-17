020cc3e6e72b46472aa387202fbaf6df5906a047
CMHyuk <alsgur9710@naver.com>
2023-05-17 23:16:16 +0900

    feat : FindByEmail

--- src/main/java/com/ottention/banana/repository/UserRepository.java
+++ src/main/java/com/ottention/banana/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.ottention.banana.repository;
 import com.ottention.banana.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
