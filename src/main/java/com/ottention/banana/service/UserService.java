587c740d7c6efddaddd6bf8cc3bd528960c99b38
CMHyuk <alsgur9710@naver.com>
2023-05-17 23:17:49 +0900

    feat : Save user function from google

--- src/main/java/com/ottention/banana/service/UserService.java
+++ src/main/java/com/ottention/banana/service/UserService.java
@@ -2,6 +2,7 @@ package com.ottention.banana.service;
 
 import com.ottention.banana.entity.User;
 import com.ottention.banana.repository.UserRepository;
+import com.ottention.banana.response.google.GoogleOAuth2UserInfo;
 import com.ottention.banana.response.kakao.GetMemberInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,8 +20,21 @@ public class UserService {
                 .nickName(userInfo.getKakaoAccount().getProfile().getNickname())
                 .email(userInfo.getKakaoAccount().getEmail())
                 .build();
+        return saveUser(user);
+    }
 
-        return userRepository.save(user).getId();
+    public Long saveFromGoogle(GoogleOAuth2UserInfo userInfo) {
+        User user = User.builder()
+                .nickName(userInfo.getName())
+                .email(userInfo.getEmail())
+                .build();
+        return saveUser(user);
+    }
+
+    public Long saveUser(User user) {
+        return userRepository.findByEmail(user.getEmail())
+                .map(User::getId)
+                .orElseGet(() -> userRepository.save(user).getId());
     }
 
 }
