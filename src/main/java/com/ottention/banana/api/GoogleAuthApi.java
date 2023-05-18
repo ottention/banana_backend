88f6c66b1a427159a3817142cf8b5fb36753a2c9
CMHyuk <alsgur9710@naver.com>
2023-05-18 19:23:50 +0900

    refactor : Rename from AppConfig

--- src/main/java/com/ottention/banana/api/GoogleAuthApi.java
+++ src/main/java/com/ottention/banana/api/GoogleAuthApi.java
@@ -1,6 +1,6 @@
 package com.ottention.banana.api;
 
-import com.ottention.banana.config.AppConfig;
+import com.ottention.banana.config.SocialLoginConfig;
 import com.ottention.banana.response.google.GoogleOAuth2UserInfo;
 import com.ottention.banana.response.google.GoogleOAuthResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +14,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @RequiredArgsConstructor
 public class GoogleAuthApi {
 
-    private final AppConfig appConfig;
+    private final SocialLoginConfig socialLoginConfig;
 
     public GoogleOAuth2UserInfo getMemberInfo(String idToken) {
-        WebClient webClient = appConfig.google();
+        WebClient webClient = socialLoginConfig.google();
 
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.add("id_token", idToken);
