8c712774b174f57977421803e74d2381f6efcad0
CMHyuk <alsgur9710@naver.com>
2023-05-18 20:02:31 +0900

    feat : SaveBusinessCardRequest

---
+++ src/main/java/com/ottention/banana/request/SaveBusinessCardRequest.java
@@ -0,0 +1,16 @@
+package com.ottention.banana.request;
+
+import com.ottention.banana.response.ContentResponse;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class SaveBusinessCardRequest {
+
+    private Boolean isPublic;
+    private List<ContentResponse> contents;
+    private List<String> tags;
+
+}
