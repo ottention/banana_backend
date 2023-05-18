e60107af1d86b9d042b25c9bfe9a5bf5c86f0b0d
CMHyuk <alsgur9710@naver.com>
2023-05-18 20:03:14 +0900

    feat : ImageResponse

---
+++ src/main/java/com/ottention/banana/response/ImageResponse.java
@@ -0,0 +1,10 @@
+package com.ottention.banana.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ImageResponse {
+    private final String imageUrl;
+}
