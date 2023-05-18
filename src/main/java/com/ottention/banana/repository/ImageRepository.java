84b83de3631badc72ea62bbc3418846b94563bbd
CMHyuk <alsgur9710@naver.com>
2023-05-18 19:56:25 +0900

    feat : ImageRepository

---
+++ src/main/java/com/ottention/banana/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.ottention.banana.repository;
+
+import com.ottention.banana.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findByBusinessCardId(Long id);
+}
