37381f294d5c9f321429c2bc6c13e0fac5504ae2
CMHyuk <alsgur9710@naver.com>
2023-05-18 19:55:25 +0900

    feat : BusinessCardTagRepository

---
+++ src/main/java/com/ottention/banana/repository/BusinessCardTagRepository.java
@@ -0,0 +1,10 @@
+package com.ottention.banana.repository;
+
+import com.ottention.banana.entity.BusinessCardTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BusinessCardTagRepository extends JpaRepository<BusinessCardTag, Long> {
+    List<BusinessCardTag> findByBusinessCardId(Long id);
+}
