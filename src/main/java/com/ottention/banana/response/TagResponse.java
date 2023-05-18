517e4fb42009916443a35959e8d9af7766b6fb80
CMHyuk <alsgur9710@naver.com>
2023-05-18 20:02:43 +0900

    feat : TagResponse

---
+++ src/main/java/com/ottention/banana/response/TagResponse.java
@@ -0,0 +1,10 @@
+package com.ottention.banana.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TagResponse {
+    private final String tag;
+}
