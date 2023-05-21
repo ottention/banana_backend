8f152a9e4af2065ff59f2208ac6da3ad6945e3d4
CMHyuk <alsgur9710@naver.com>
2023-05-21 15:18:28 +0900

    test : JwtService logic test - generate, validate

---
+++ src/test/java/com/ottention/banana/service/JwtServiceTest.java
@@ -0,0 +1,71 @@
+package com.ottention.banana.service;
+
+import com.ottention.banana.config.JwtConfig;
+import com.ottention.banana.exception.jwt.AccessTokenNotFoundException;
+import com.ottention.banana.exception.jwt.RefreshTokenNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class JwtServiceTest {
+
+    private JwtService jwtService;
+    private JwtConfig jwtConfig;
+
+    @BeforeEach
+    void setUp() {
+        jwtConfig = new JwtConfig();
+        jwtConfig.setJwtKey("secretKey");
+        jwtService = new JwtService(jwtConfig);
+    }
+
+    @Test
+    @DisplayName("AccessToken 생성 테스트")
+    void generateAccessToken_shouldGenerateValidAccessToken() {
+        //given
+        Long userId = 123L;
+
+        //when
+        String accessToken = jwtService.generateAccessToken(userId);
+
+        //then
+        assertNotNull(accessToken);
+    }
+
+    @Test
+    @DisplayName("refreshToken 생성 테스트")
+    void generateRefreshToken_shouldGenerateValidRefreshToken() {
+        //given
+        Long userId = 123L;
+
+        //when
+        String refreshToken = jwtService.generateRefreshToken(userId);
+
+        //then
+        assertNotNull(refreshToken);
+    }
+
+    @Test
+    @DisplayName("AccessToken 예외(null) 테스트")
+    void validateAccessToken_shouldThrowExceptionForInvalidToken() {
+        //given
+        String accessToken = null;
+
+        //then
+        assertThrows(AccessTokenNotFoundException.class, () -> jwtService.validateAccessToken(accessToken));
+    }
+
+    @Test
+    @DisplayName("RefreshToken 예외(null) 테스트")
+    void validateRefreshToken_shouldThrowExceptionForInvalidToken() {
+        //given
+        String refreshToken = null;
+
+        //then
+        assertThrows(RefreshTokenNotFoundException.class, () -> jwtService.reissueAccessToken(refreshToken));
+    }
+
+}
