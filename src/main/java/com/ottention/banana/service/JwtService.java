6dff014f570fac11a4b27423b0a4caa0fc0a6990
CMHyuk <alsgur9710@naver.com>
2023-05-21 15:13:20 +0900

    refactor : Change accessToken expired time, parameter name & Replace with custom error class

--- src/main/java/com/ottention/banana/service/JwtService.java
+++ src/main/java/com/ottention/banana/service/JwtService.java
@@ -1,8 +1,12 @@
 package com.ottention.banana.service;
 
 import com.ottention.banana.config.JwtConfig;
+import com.ottention.banana.exception.jwt.AccessTokenNotFoundException;
+import com.ottention.banana.exception.jwt.RefreshTokenExpiredException;
+import com.ottention.banana.exception.jwt.RefreshTokenNotFoundException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +20,7 @@ import java.util.Date;
 public class JwtService {
 
     private final JwtConfig jwtConfig;
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60L * 60L * 24L * 7L; // 액세스 토큰의 만료 시간: 7일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60L * 60L * 6L; // 액세스 토큰의 만료 시간: 6시간
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60L * 60L * 24L * 30L; // 리프레시 토큰의 만료 시간: 30일
 
     public String generateAccessToken(Long userId) {
@@ -41,24 +45,36 @@ public class JwtService {
                 .compact();
     }
 
-    public Long getSubject(String jws) {
+    public Long getSubject(String token) {
         Jws<Claims> claims = Jwts.parserBuilder()
                 .setSigningKey(jwtConfig.getJwtKey())
                 .build()
-                .parseClaimsJws(jws);
+                .parseClaimsJws(token);
         return Long.parseLong(claims.getBody().getSubject());
     }
 
-    public void validateAccessToken(String jws) {
-        if (jws == null || jws.equals("")) {
-            throw new IllegalArgumentException("AccessToken이 없습니다.");
+    public String reissueAccessToken(String refreshToken) {
+        Long userId = validateRefreshToken(refreshToken);
+        return generateAccessToken(userId);
+    }
+
+    public void validateAccessToken(String accessToken) {
+        if (accessToken == null || accessToken.equals("")) {
+            throw new AccessTokenNotFoundException();
         }
     }
 
-    public void validateRefreshToken(String refreshToken) {
+    private Long validateRefreshToken(String refreshToken) {
         if (refreshToken == null || refreshToken.equals("")) {
-            throw new RuntimeException("RefreshToken이 없습니다.");
+            throw new RefreshTokenNotFoundException();
         }
+
+        try {
+            return getSubject(refreshToken);
+        } catch (JwtException e) {
+            throw new RefreshTokenExpiredException();
+        }
+
     }
 
     private SecretKey getSecretKey() {
