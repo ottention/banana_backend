e9577279e9c7372e7e20807ea3773a22eb82d5fc
CMHyuk <alsgur9710@naver.com>
2023-05-21 15:17:40 +0900

    feat : JwtController - reissue accessToken

---
+++ src/main/java/com/ottention/banana/controller/JwtController.java
@@ -0,0 +1,22 @@
+package com.ottention.banana.controller;
+
+import com.ottention.banana.response.jwt.ReissueResponse;
+import com.ottention.banana.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JwtController {
+
+    private final JwtService jwtService;
+
+    @PostMapping("/reissue")
+    public ReissueResponse reissueToken(@RequestHeader("RefreshToken") String refreshToken) {
+        String reissuedAccessToken = jwtService.reissueAccessToken(refreshToken);
+        return new ReissueResponse(reissuedAccessToken);
+    }
+
+}
