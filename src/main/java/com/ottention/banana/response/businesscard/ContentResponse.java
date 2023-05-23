6d571151dc4c1e7a918a4980fa676ee6110c420b
CMHyuk <alsgur9710@naver.com>
2023-05-23 22:04:08 +0900

    feat : Add new field to ContentResponse - width, height

--- src/main/java/com/ottention/banana/response/businesscard/ContentResponse.java
+++ src/main/java/com/ottention/banana/response/businesscard/ContentResponse.java
@@ -7,26 +7,40 @@ import lombok.NoArgsConstructor;
 public class ContentResponse {
 
     private String content;
+    private int width;
+    private int height;
     private int xAxis;
     private int yAxis;
 
-    public static ContentResponse toContent(String content, int xAxis, int yAxis) {
+    public static ContentResponse toContent(String content, int width, int height, int xAxis, int yAxis) {
         return ContentResponse.builder()
                 .content(content)
+                .width(width)
+                .height(height)
                 .xAxis(xAxis)
                 .yAxis(yAxis)
                 .build();
     }
 
     @Builder
-    public ContentResponse(String content, int xAxis, int yAxis) {
+    public ContentResponse(String content, int width, int height, int xAxis, int yAxis) {
         this.content = content;
+        this.width = width;
+        this.height = height;
         this.xAxis = xAxis;
         this.yAxis = yAxis;
     }
 
     public String getContent() {
         return content;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public int getxAxis() {
