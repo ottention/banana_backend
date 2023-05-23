beea53bb75c408e10b4945e1202471d9605b6606
CMHyuk <alsgur9710@naver.com>
2023-05-23 22:02:25 +0900

    feat : Add new column to BusinessCardContent - width, height

--- src/main/java/com/ottention/banana/entity/BusinessCardContent.java
+++ src/main/java/com/ottention/banana/entity/BusinessCardContent.java
@@ -18,12 +18,13 @@ import static lombok.AccessLevel.PROTECTED;
 @EntityListeners(AuditingEntityListener.class)
 public class BusinessCardContent {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
+    @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name = "content_id")
     private Long id;
 
     private String content;
+    private int width;
+    private int height;
     private int xAxis;
     private int yAxis;
     private boolean isFront;
@@ -38,9 +39,23 @@ public class BusinessCardContent {
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 
+    public static BusinessCardContent createBusinessCardContent(String content, int width, int height,
+                                                                int xAxis, int yAxis, boolean isFront) {
+        return BusinessCardContent.builder()
+                .content(content)
+                .width(width)
+                .height(height)
+                .xAxis(xAxis)
+                .yAxis(yAxis)
+                .isFront(isFront)
+                .build();
+    }
+
     @Builder
-    public BusinessCardContent(String content, int xAxis, int yAxis, boolean isFront) {
+    public BusinessCardContent(String content, int width, int height, int xAxis, int yAxis, boolean isFront) {
         this.content = content;
+        this.width = width;
+        this.height = height;
         this.xAxis = xAxis;
         this.yAxis = yAxis;
         this.isFront = isFront;
