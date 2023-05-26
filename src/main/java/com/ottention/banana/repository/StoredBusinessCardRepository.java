10fb17ec9256ada133fa09055dda56d417164882
yeonjy <yeonjypaper@gmail.com>
2023-05-26 09:46:49 +0900

    feat: 저장한 명함 저장한 사용자 ID로 찾기 기능 추가

--- src/main/java/com/ottention/banana/repository/StoredBusinessCardRepository.java
+++ src/main/java/com/ottention/banana/repository/StoredBusinessCardRepository.java
@@ -2,7 +2,12 @@ package com.ottention.banana.repository;
 
 import com.ottention.banana.entity.StoredBusinessCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StoredBusinessCardRepository extends JpaRepository<StoredBusinessCard, Long> {
+    List<StoredBusinessCard> findByUserId(Long userId);
 }
 
