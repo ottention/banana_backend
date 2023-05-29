769e5b90f41a4450706a8294d57c6d73e33a4f8d
CMHyuk <alsgur9710@naver.com>
2023-05-29 23:12:25 +0900

    refactor : BaseEntity 상속

--- src/main/java/com/ottention/banana/entity/StoredBusinessCard.java
+++ src/main/java/com/ottention/banana/entity/StoredBusinessCard.java
@@ -1,38 +1,27 @@
 package com.ottention.banana.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 @Getter
-public class StoredBusinessCard {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@NoArgsConstructor(access = PROTECTED)
+public class StoredBusinessCard extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name = "stored_business_card_id")
     private Long id;
 
     private String name;  //명함 저장명
     private Boolean isBookmarked;  //즐겨찾기 여부
 
-    @CreatedDate
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
-
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "card_id")
+    @JoinColumn(name = "business_card_id")
     private BusinessCard businessCard;
 
     @ManyToOne(fetch = LAZY)
@@ -42,4 +31,5 @@ public class StoredBusinessCard {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
     private User user;  //명함 저장한 사람
+
 }
