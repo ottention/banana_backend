53843ac8a8fdda62f9fd793a8c0fbdf6c6727cec
CMHyuk <alsgur9710@naver.com>
2023-05-29 23:07:52 +0900

    refactor : BaseEntity 상속 & pk 컬럼 값 이름 변경

--- src/main/java/com/ottention/banana/entity/BusinessCard.java
+++ src/main/java/com/ottention/banana/entity/BusinessCard.java
@@ -4,35 +4,22 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
-public class BusinessCard {
+public class BusinessCard extends BaseEntity {
 
     @Id @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "card_id")
+    @Column(name = "business_card_id")
     private Long id;
 
     private Boolean isPublic; //공개 비공개
     private Boolean isRepresent; //대표 명함
     private int likeCount; //좋아요 수
-
-    @CreatedDate
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
 
     @OneToOne
     @JoinColumn(name = "qrcode_id")
