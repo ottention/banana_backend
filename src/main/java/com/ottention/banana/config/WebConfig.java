65e15308080a8eb4affba296bb8dd7ab0ca88733
CMHyuk <alsgur9710@naver.com>
2023-06-04 13:23:37 +0900

    feat : 인터셉터 추가 - API 문서 제외 모든 URL 적용

--- src/main/java/com/ottention/banana/config/WebConfig.java
+++ src/main/java/com/ottention/banana/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.ottention.banana.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,7 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new JwtArgumentResolver(jwtService));
+        resolvers.add(new JwtArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor(jwtService))
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/docs/**");
     }
 
 }
