55fe97fd140000c091b105b1ca8d176c224086d5
CMHyuk <alsgur9710@naver.com>
2023-06-04 13:22:35 +0900

    refactor : 토큰 검증 제거 & 인터셉터에서 넘겨준 사용자 정보 LoginUser에 담아 리턴

--- src/main/java/com/ottention/banana/config/JwtArgumentResolver.java
+++ src/main/java/com/ottention/banana/config/JwtArgumentResolver.java
@@ -1,10 +1,7 @@
 package com.ottention.banana.config;
 
-import com.ottention.banana.exception.jwt.Unauthorized;
 import com.ottention.banana.request.LoginUser;
-import com.ottention.banana.service.JwtService;
-import io.jsonwebtoken.JwtException;
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -13,10 +10,7 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Slf4j
-@RequiredArgsConstructor
 public class JwtArgumentResolver implements HandlerMethodArgumentResolver {
-
-    private final JwtService jwtService;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -29,16 +23,10 @@ public class JwtArgumentResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         log.info("resolveArgument 실행");
-        String accessToken = webRequest.getHeader("Authorization");
-        jwtService.validateAccessToken(accessToken);
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Long userId = (Long) request.getAttribute("userId");
 
-        try {
-            Long userId = jwtService.getSubject(accessToken);
-            log.info("userId = {}", userId);
-            return new LoginUser(userId);
-        } catch (JwtException e) {
-            throw new Unauthorized();
-        }
-
+        log.info("userId = {}", userId);
+        return new LoginUser(userId);
     }
 }
