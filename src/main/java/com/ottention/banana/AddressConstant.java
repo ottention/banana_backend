964a8813465d201e1f970e117b590795f6fa9c6a
CMHyuk <alsgur9710@naver.com>
2023-06-07 12:53:02 +0900

    remove : AddressConstant 클래스 변환으로 삭제

---
+++ src/main/java/com/ottention/banana/AddressConstant.java
@@ -0,0 +1,6 @@
+package com.ottention.banana;
+
+public final class AddressConstant {
+    //배포할 땐 http://3.37.86.26:8080/businessCard/
+    public static final String ADDRESS = "http://localhost:8080/businessCard/";
+}
