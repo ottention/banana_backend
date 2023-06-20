3f0eea8b41b8795d6d2eb0749b53329e3300e852
CMHyuk <alsgur9710@naver.com>
2023-06-20 16:57:09 +0900

    feat : 입력 값 예외처리 BindException 추가

--- src/main/java/com/ottention/banana/controller/ExceptionController.java
+++ src/main/java/com/ottention/banana/controller/ExceptionController.java
@@ -1,10 +1,19 @@
 package com.ottention.banana.controller;
 
+import com.ottention.banana.dto.response.error.BindErrorResponse;
 import com.ottention.banana.exception.Exception;
 import com.ottention.banana.dto.response.error.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestControllerAdvice
 public class ExceptionController {
@@ -20,6 +29,18 @@ public class ExceptionController {
 
         return ResponseEntity.status(statusCode)
                 .body(body);
+    }
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public BindErrorResponse BindException(BindException bindException) {
+        List<FieldError> errors = bindException.getFieldErrors();
+
+        return BindErrorResponse.builder()
+                .message(errors.stream()
+                        .map(e -> e.getDefaultMessage())
+                        .collect(Collectors.toList()))
+                .build();
     }
 
 }
