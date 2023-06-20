bfe8cd91885aaac788d4b99087a9797e563acdd3
CMHyuk <alsgur9710@naver.com>
2023-06-20 16:59:27 +0900

    feat : 방명록 예외 클래스 - 자신의 명함에 방명록 입력

---
+++ src/main/java/com/ottention/banana/exception/SelfGuestbookNotAllowedException.java
@@ -0,0 +1,16 @@
+package com.ottention.banana.exception;
+
+public class SelfGuestbookNotAllowedException extends Exception {
+
+    private static final String MESSAGE = "자기 자신의 명함에는 방명록을 남길 수 없습니다.";
+
+    public SelfGuestbookNotAllowedException() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatus() {
+        return 403;
+    }
+
+}
