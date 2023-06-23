b75c6e6480a8e16a9b78cc07c805fd8b5d36b7bd
yeonjy <yeonjypaper@gmail.com>
2023-06-24 00:16:32 +0900

    feat: Bookmarked Stored BusinessCard 전체 조회

--- src/main/java/com/ottention/banana/repository/StoredBusinessCardRepository.java
+++ src/main/java/com/ottention/banana/repository/StoredBusinessCardRepository.java
@@ -1,6 +1,7 @@
 package com.ottention.banana.repository;
 
 import com.ottention.banana.entity.StoredBusinessCard;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import java.util.List;
 public interface StoredBusinessCardRepository extends JpaRepository<StoredBusinessCard, Long> {
     List<StoredBusinessCard> findAllByUserIdOrderByModifiedDateDesc(Long userId);
     List<StoredBusinessCard> findTop2ByUserIdOrderByModifiedDateDesc(Long userId);
+    List<StoredBusinessCard> findAllByUserIdAndIsBookmarkedTrueOrderByModifiedDateDesc(Long userId, Pageable pageable);
 }
 
