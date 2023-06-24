f6cb76b5feae72b0f635ce7dadb36d8fe1cbe319
CMHyuk <alsgur9710@naver.com>
2023-06-24 21:26:04 +0900

    feat : Coordinate 추가

--- src/main/java/com/ottention/banana/entity/Image.java
+++ src/main/java/com/ottention/banana/entity/Image.java
@@ -21,23 +21,27 @@ public class Image extends BaseEntity {
     private String imageUrl; //이미지 주소
     private Boolean isFront; //앞, 뒤 구분
 
+    @Embedded
+    private Coordinate coordinate;
+
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "business_card_id")
     private BusinessCard businessCard;
 
-    public static Image createImage(String imageUrl, Boolean isFront, BusinessCard businessCard) {
+    public static Image createImage(String imageUrl, Boolean isFront, BusinessCard businessCard, Coordinate coordinate) {
         return Image.builder()
                 .imageUrl(imageUrl)
                 .isFront(isFront)
                 .businessCard(businessCard)
+                .coordinate(coordinate)
                 .build();
     }
 
     @Builder
-    public Image(String imageUrl, Boolean isFront, BusinessCard businessCard) {
+    public Image(String imageUrl, Boolean isFront, Coordinate coordinate, BusinessCard businessCard) {
         this.imageUrl = imageUrl;
         this.isFront = isFront;
+        this.coordinate = coordinate;
         this.businessCard = businessCard;
     }
-
 }
