e8589088ffa1d3bba0fc8846000f7507e2d453d9
yeonjy <yeonjypaper@gmail.com>
2023-06-25 07:11:07 +0900

    rename: service 메서드명 변경으로 인한 수정

--- src/main/java/com/ottention/banana/controller/WalletController.java
+++ src/main/java/com/ottention/banana/controller/WalletController.java
@@ -3,32 +3,36 @@ package com.ottention.banana.controller;
 import com.ottention.banana.config.Login;
 import com.ottention.banana.dto.request.LoginUser;
 import com.ottention.banana.dto.response.businesscard.StoredCardResponse;
-import com.ottention.banana.service.WalletService;
+import com.ottention.banana.service.CategoryService;
+import com.ottention.banana.service.StoredBusinessCardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/banana/wallet")
 public class WalletController {
 
-    private final WalletService walletService;
+    private final StoredBusinessCardService storedBusinessCardService;
+    private final CategoryService categoryService;
 
+
+    //전체명함
     @GetMapping("")
     public ResponseEntity<Map<String, Object>> getAllStoredCards(@Login LoginUser user) {
         Map<String, Object> result = new HashMap<>();
-        result.put("twoBookmarkedStoredCards", walletService.getTwoBookmarkedCards(user.getId()));
-        result.put("allStoredCards", walletService.getAllStoredBusinessCards(user.getId()));
+        result.put("twoBookmarkedStoredCards", storedBusinessCardService.getTwoBookmarkedCards(user.getId()));
+        result.put("allStoredCards", storedBusinessCardService.getAllStoredBusinessCards(user.getId()));
 
         return ResponseEntity.ok(result);
     }
-
 }
