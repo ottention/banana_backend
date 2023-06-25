b2b3cd9255b368b19e4de950cf9ae55545d0674b
CMHyuk <alsgur9710@naver.com>
2023-06-25 16:57:27 +0900

    refactor : @Setter 추가

--- src/main/java/com/ottention/banana/dto/request/SaveTagRequest.java
+++ src/main/java/com/ottention/banana/dto/request/SaveTagRequest.java
@@ -1,10 +1,11 @@
 package com.ottention.banana.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Getter
+@Getter @Setter
 public class SaveTagRequest {
     private List<String> tags;
 }
