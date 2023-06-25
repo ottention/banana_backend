f42939f3c87a8a21069582170c6c2cee498a88c1
CMHyuk <alsgur9710@naver.com>
2023-06-25 16:57:09 +0900

    feat : 태그 저장, 조회 기능

---
+++ src/main/java/com/ottention/banana/service/TagService.java
@@ -0,0 +1,46 @@
+package com.ottention.banana.service;
+
+import com.ottention.banana.dto.request.SaveTagRequest;
+import com.ottention.banana.dto.response.businesscard.TagResponse;
+import com.ottention.banana.entity.BusinessCard;
+import com.ottention.banana.entity.BusinessCardTag;
+import com.ottention.banana.entity.Tag;
+import com.ottention.banana.repository.BusinessCardTagRepository;
+import com.ottention.banana.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class TagService {
+
+    private final TagRepository tagRepository;
+    private final BusinessCardTagRepository businessCardTagRepository;
+
+    @Transactional
+    public void saveTag(SaveTagRequest request, BusinessCard businessCard) {
+        List<String> tags = request.getTags();
+        for (String tag : tags) {
+            BusinessCardTag businessCardTag = new BusinessCardTag();
+            Tag createdTag = new Tag();
+
+            createdTag.updateName(tag);
+            createdTag.addTag(businessCardTag, businessCard);
+
+            businessCardTagRepository.save(businessCardTag);
+            tagRepository.save(createdTag);
+        }
+    }
+
+    public List<TagResponse> getTags(Long businessCardId) {
+        List<BusinessCardTag> businessCardTags = businessCardTagRepository.findByBusinessCardId(businessCardId);
+        return businessCardTags.stream().map(t -> new TagResponse(t.getTag().getName()))
+                .collect(Collectors.toList());
+    }
+
+}
