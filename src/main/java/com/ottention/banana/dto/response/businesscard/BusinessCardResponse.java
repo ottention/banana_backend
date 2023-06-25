577030f974ee9c73a0e362c4e9a37018d4063fae
CMHyuk <alsgur9710@naver.com>
2023-06-25 16:58:13 +0900

    feat : 태그 반환 값 추가

--- src/main/java/com/ottention/banana/dto/response/businesscard/BusinessCardResponse.java
+++ src/main/java/com/ottention/banana/dto/response/businesscard/BusinessCardResponse.java
@@ -15,21 +15,25 @@ public class BusinessCardResponse {
 
     private final List<BusinessCardContentDto> contents;
     private final List<ImageResponse> images;
+    private final List<TagResponse> tags;
 
-    public static BusinessCardResponse toBusinessCard(List<BusinessCardContent> businessCardContents, List<Image> images) {
+    public static BusinessCardResponse toBusinessCard(List<BusinessCardContent> businessCardContents, List<Image> images,
+                                                      List<TagResponse> tags) {
         return BusinessCardResponse.builder()
                 .contents(businessCardContents.stream().map(c ->
                         BusinessCardContentDto.toContent(c))
                         .collect(toList()))
                 .images(images.stream().map(i -> new ImageResponse(i.getImageUrl(), i.getCoordinate()))
                         .collect(toList()))
+                .tags(tags)
                 .build();
     }
 
     @Builder
-    public BusinessCardResponse(List<BusinessCardContentDto> contents, List<ImageResponse> images) {
+    public BusinessCardResponse(List<BusinessCardContentDto> contents, List<ImageResponse> images, List<TagResponse> tags) {
         this.contents = contents;
         this.images = images;
+        this.tags = tags;
     }
 
 }
