2fca8151b9bb82ae7eace790998ec50c6e0aeb0c
yeonjy <yeonjypaper@gmail.com>
2023-06-27 02:31:42 +0900

    refactor: Builder 패턴 적용

--- src/main/java/com/ottention/banana/service/wallet/DetailStoredBusinessCardService.java
+++ src/main/java/com/ottention/banana/service/wallet/DetailStoredBusinessCardService.java
@@ -1,12 +1,13 @@
 package com.ottention.banana.service.wallet;
 
-import com.ottention.banana.dto.response.businesscard.BusinessCardResponse;
+import com.ottention.banana.dto.response.businesscard.StoredBusinessCardResponse;
 import com.ottention.banana.entity.StoredBusinessCard;
 import com.ottention.banana.repository.StoredBusinessCardRepository;
-import com.ottention.banana.service.BusinessCardService;
+import com.ottention.banana.service.BusinessCardContentService;
+import com.ottention.banana.service.ImageService;
+import com.ottention.banana.service.TagService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,19 +16,22 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DetailStoredBusinessCardService {
     private final StoredBusinessCardRepository storedCardRepository;
-    private final BusinessCardService businessCardService;
+    private final BusinessCardContentService businessCardContentService;
+    private final ImageService imageService;
+    private final TagService tagService;
 
-    public BusinessCardResponse findStoredBusinessCardFront(Long id) {
-        return businessCardService.getFrontBusinessCard(getBusinessCardId(id));
-    }
-
-    public BusinessCardResponse findStoredBusinessCardBack(Long id) {
-        return businessCardService.getBackBusinessCard(getBusinessCardId(id));
-    }
-
-    public Long getBusinessCardId(Long id) {
+    public StoredBusinessCardResponse findStoredBusinessCard(Long id, Boolean isFront) {
         StoredBusinessCard storedBusinessCard = storedCardRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        return storedBusinessCard.getBusinessCard().getId();
+        return toResponse(storedBusinessCard, isFront);
     }
 
+    public StoredBusinessCardResponse toResponse(StoredBusinessCard storedCard, Boolean isFront) {
+        return StoredBusinessCardResponse.builder()
+                .id(storedCard.getId())
+                .name(storedCard.getName())
+                .contents(businessCardContentService.findByBusinessCardIdAndIsFront(storedCard.getBusinessCard().getId(), isFront))
+                .images(imageService.findByBusinessCardIdAndIsFront(storedCard.getBusinessCard().getId(), isFront))
+                .tags(tagService.getTags(storedCard.getBusinessCard().getId()))
+                .build();
+    }
 }
