4ae8a80bece61461d25c892ad67a2564c0053848
yeonjy <yeonjypaper@gmail.com>
2023-06-27 02:36:18 +0900

    feat: Pageable 파라미터 추가

--- src/main/java/com/ottention/banana/repository/NoteRepository.java
+++ src/main/java/com/ottention/banana/repository/NoteRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
-    List<Note> findAllBy(Long storedBusinessCardId, Pageable pageable);
+    List<Note> findAllByStoredBusinessCardId(Long storedBusinessCardId, Pageable pageable);
 }
