ce5d4ea18938efa02c8ed13887f5fc926565a287
CMHyuk <alsgur9710@naver.com>
2023-06-27 16:10:49 +0900

    feat : 좋아요 중복 에러 클래스

---
+++ src/main/java/com/ottention/banana/exception/DuplicationLikeException.java
@@ -0,0 +1,16 @@
+package com.ottention.banana.exception;
+
+public class DuplicationLikeException extends Exception {
+
+    private static final String MESSAGE = "이미 좋아요를 누른 게시글입니다.";
+
+    public DuplicationLikeException() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public int getStatus() {
+        return 409;
+    }
+
+}
