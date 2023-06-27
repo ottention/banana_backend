7ffaef966eb31435e5765e3426c87f238bc2f5f7
CMHyuk <alsgur9710@naver.com>
2023-06-27 16:12:19 +0900

    feat : 명함 좋아요, 취소 컨트롤러

---
+++ src/main/java/com/ottention/banana/controller/BusinessCardLikeController.java
@@ -0,0 +1,28 @@
+package com.ottention.banana.controller;
+
+import com.ottention.banana.config.Login;
+import com.ottention.banana.dto.request.LoginUser;
+import com.ottention.banana.service.BusinessCardLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BusinessCardLikeController {
+
+    private final BusinessCardLikeService businessCardLikeService;
+
+    @PostMapping("/businessCard/{businessCardId}/like")
+    public int like(@Login LoginUser loginUser, @PathVariable Long businessCardId) {
+        return businessCardLikeService.like(loginUser.getId(), businessCardId);
+    }
+
+    @DeleteMapping("/businessCard/{businessCardId}/cancelLike")
+    public int cancelLike(@Login LoginUser loginUser, @PathVariable Long businessCardId) {
+        return businessCardLikeService.cancelLike(loginUser.getId(), businessCardId);
+    }
+
+}
