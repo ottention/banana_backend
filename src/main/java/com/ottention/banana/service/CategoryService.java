dda4644302d3f5439bec82c5da9f444ef7bbdb8e
yeonjy <yeonjypaper@gmail.com>
2023-06-29 00:29:00 +0900

    feat: CategoryCardsResponse에 Mapper패턴 적용

--- src/main/java/com/ottention/banana/service/CategoryService.java
+++ src/main/java/com/ottention/banana/service/CategoryService.java
@@ -1,12 +1,18 @@
 package com.ottention.banana.service;
 
+import com.ottention.banana.dto.response.businesscard.CategoryCardsResponse;
 import com.ottention.banana.dto.response.businesscard.CategoryResponse;
 import com.ottention.banana.entity.Category;
+import com.ottention.banana.mapper.CategoryCardsMapper;
+import com.ottention.banana.mapper.CategoryMapper;
 import com.ottention.banana.repository.CategoryRepository;
+import com.ottention.banana.service.wallet.StoredBusinessCardService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,18 +22,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+    private final CategoryCardsMapper categoryCardsMapper;
+    private final StoredBusinessCardService storedCardService;
 
     //카테고리 조회
     public List<CategoryResponse> getCategories(Long id) {
         List<Category> categories = categoryRepository.findAllByUserIdOrderByModifiedDateDesc(id);
         List<CategoryResponse> categoryResponses = new ArrayList<>();
 
-        categories.forEach(c -> categoryResponses.add(new CategoryResponse(c)));
+        categories.forEach(c -> categoryResponses.add(categoryMapper.toResponse(c)));
         return categoryResponses;
     }
 
-    public String getCategoryName(long categoryId) {
+    private String getCategoryName(Long categoryId) {
         Category category = categoryRepository.findById(categoryId).orElseThrow(EntityNotFoundException::new);
         return category.getCategoryName();
+    }
+
+    public CategoryCardsResponse getCategoryCards(Long categoryId, Pageable pageable) {
+        return categoryCardsMapper.toResponse(getCategoryName(categoryId), storedCardService.getStoredCardByCategory(categoryId, pageable));
     }
 }
