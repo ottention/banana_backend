a7d5e7e622c189fb759ad200daf96d9a621742e4
yeonjy <yeonjypaper@gmail.com>
2023-07-03 06:24:10 +0900

    feat: Notification Response DTO 구현

---
+++ src/main/java/com/ottention/banana/dto/response/NotificationResponse.java
@@ -0,0 +1,28 @@
+package com.ottention.banana.dto.response;
+
+import com.ottention.banana.entity.notification.Notification;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class NotificationResponse {
+    private Long id;
+    private String content;
+    private String url;
+    private Boolean isRead;
+    private LocalDateTime createdAt;
+
+    @Builder
+    public NotificationResponse(Notification notification) {
+        this.id = notification.getId();
+        this.content = notification.getContent().toString();
+        this.url = notification.getUrl().getUrl();
+        this.isRead = notification.getIsRead();
+        this.createdAt = notification.getCreatedDate();
+
+    }
+}
