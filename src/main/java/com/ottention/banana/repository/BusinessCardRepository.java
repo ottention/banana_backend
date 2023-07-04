9843f0dffd2569a0773c024b039bfb9603dcd38b
CMHyuk <alsgur9710@naver.com>
2023-07-04 23:37:13 +0900

    feat : UserId로의 명함 조회 기능

--- src/main/java/com/ottention/banana/repository/BusinessCardRepository.java
+++ src/main/java/com/ottention/banana/repository/BusinessCardRepository.java
@@ -3,5 +3,8 @@ package com.ottention.banana.repository;
 import com.ottention.banana.entity.BusinessCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BusinessCardRepository extends JpaRepository<BusinessCard, Long> {
+    List<BusinessCard> findByUserId(Long userId);
 }
