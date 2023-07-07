80b6bddfd0b8f0b524c6ec268082e7e43adf602f
yeonjy <yeonjypaper@gmail.com>
2023-07-08 00:26:50 +0900

    feat: NotificationRepository 알림 삭제 기능 구현

--- src/main/java/com/ottention/banana/repository/notification/NotificationRepository.java
+++ src/main/java/com/ottention/banana/repository/notification/NotificationRepository.java
@@ -1,11 +1,11 @@
 package com.ottention.banana.repository.notification;
 
-import com.ottention.banana.entity.User;
 import com.ottention.banana.entity.notification.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    List<Notification> findAllByUser(User user);  //알림 전체 조회
+    List<Notification> findAllByUserId(Long userId);  //알림 전체 조회
+    void deleteAllByUserId(Long userId);  //알림 전체 삭제
 }
