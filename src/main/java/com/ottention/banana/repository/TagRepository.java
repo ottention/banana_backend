4092f9391f28e1118ede2446645b3c2d6718d9b0
CMHyuk <alsgur9710@naver.com>
2023-07-08 10:00:19 +0900

    feat : 탑 10 태그 조회 기능

--- src/main/java/com/ottention/banana/repository/TagRepository.java
+++ src/main/java/com/ottention/banana/repository/TagRepository.java
@@ -2,6 +2,11 @@ package com.ottention.banana.repository;
 
 import com.ottention.banana.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    @Query("SELECT t.name FROM Tag t GROUP BY t.name ORDER BY COUNT(t) DESC")
+    List<String> findTop10Tags();
 }
