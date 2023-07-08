e68e5046eaef3b7818c6b50cc2a474b1be79faa0
CMHyuk <alsgur9710@naver.com>
2023-07-08 10:01:33 +0900

    feat : 탑 10 명함 조회 기능

--- src/main/java/com/ottention/banana/repository/BusinessCardRepository.java
+++ src/main/java/com/ottention/banana/repository/BusinessCardRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface BusinessCardRepository extends JpaRepository<BusinessCard, Long> {
     List<BusinessCard> findByUserId(Long userId);
+    List<BusinessCard> findTop10ByIsPublicOrderByLikeCountDesc(boolean isPublic);
 }
