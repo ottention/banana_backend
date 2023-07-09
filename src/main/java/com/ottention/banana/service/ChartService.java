f17bb2b05eb4023a53db1a6fb984e12a25c0ca64
CMHyuk <alsgur9710@naver.com>
2023-07-09 15:21:06 +0900

    feat : 실시간 탑 10 태그, 명함 조회 기능

--- src/main/java/com/ottention/banana/service/ChartService.java
+++ src/main/java/com/ottention/banana/service/ChartService.java
@@ -3,14 +3,18 @@ package com.ottention.banana.service;
 import com.ottention.banana.dto.response.businesscard.BusinessCardResponse;
 import com.ottention.banana.dto.response.businesscard.TagResponse;
 import com.ottention.banana.entity.BusinessCard;
+import com.ottention.banana.entity.TopTenTag;
 import com.ottention.banana.repository.BusinessCardRepository;
 import com.ottention.banana.repository.TagRepository;
+import com.ottention.banana.repository.TopTenTagRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -22,14 +26,33 @@ import static java.util.stream.Collectors.toList;
 public class ChartService {
 
     private final TagRepository tagRepository;
+    private final TopTenTagRepository topTenTagRepository;
     private final BusinessCardRepository businessCardRepository;
     private final BusinessCardService businessCardService;
 
     public List<TagResponse> getTopTenTags() {
-        List<String> topTenTags = tagRepository.findTop10Tags();
+        List<TopTenTag> topTenTags = topTenTagRepository.findAll();
         log.info("topTenTagsSize = {}", topTenTags.size());
-        return topTenTags.stream().map(t -> new TagResponse(t))
+        return topTenTags.stream().map(t -> new TagResponse(t.getTopTenTag()))
                 .collect(toList());
+    }
+
+    @Transactional
+    @Scheduled(fixedRate = 20000)
+    public void generateRandomTop10Tags() {
+        List<String> tags = tagRepository.findTagsWithDuplicateCounts();
+        log.info("tagsSize = {}", tags.size());
+        Collections.shuffle(tags);
+        List<String> randomTop10Tags = tags.subList(0, Math.min(10, tags.size()));
+
+        topTenTagRepository.deleteAll();
+
+        for (String tag : randomTop10Tags) {
+            log.info("tag = {}", tag);
+            TopTenTag topTenTag = new TopTenTag();
+            topTenTag.updateTopTenTag(tag);
+            topTenTagRepository.save(topTenTag);
+        }
     }
 
     public List<BusinessCardResponse> getTopTenBusinessCards(Long userId) {
