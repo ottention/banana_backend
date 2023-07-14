bcd25287ddb65bb89531956b654080156881872f
yeonjy <yeonjypaper@gmail.com>
2023-07-14 14:55:31 +0900

    test: Notification test 구현

---
+++ src/test/java/com/ottention/banana/service/event/NotificationEventListenerTest.java
@@ -0,0 +1,62 @@
+package com.ottention.banana.service.event;
+
+import com.ottention.banana.dto.request.notification.NotificationRequest;
+import com.ottention.banana.entity.User;
+import com.ottention.banana.entity.notification.NotificationType;
+import com.ottention.banana.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+
+
+@SpringBootTest
+class NotificationEventListenerTest {
+    @Autowired
+    ApplicationEventPublisher publisher;
+    @Autowired
+    NotificationEventListener listener;
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Test
+    void 이벤트_리스너_알림_send() {
+        //given
+        User user = User.builder()
+                .email("a")
+                .nickName("a")
+                .build();
+        userRepository.save(user);
+        String content = "test event listener";
+        String url = "/business/";
+
+        NotificationRequest request = NotificationRequest.builder()
+                .user(user)
+                .content(content)
+                .url(url)
+                .type(NotificationType.NEW_GUESTBOOK)
+                .build();
+
+        listener.sendPush(request);
+    }
+
+    @Test
+    void eventListener가_eventPublisher의_요청을_받는다() {
+        User user = User.builder()
+                .email("a")
+                .nickName("a")
+                .build();
+        userRepository.save(user);
+        String content = "test event listener";
+        String url = "/business/";
+        NotificationRequest request = NotificationRequest.builder()
+                .user(user)
+                .content(content)
+                .url(url)
+                .type(NotificationType.NEW_GUESTBOOK)
+                .build();
+        publisher.publishEvent(request);
+    }
+
+}
--- src/test/java/com/ottention/banana/service/notification/NotificationServiceTest.java
+++ src/test/java/com/ottention/banana/service/notification/NotificationServiceTest.java
@@ -1,23 +1,25 @@
 package com.ottention.banana.service.notification;
 
 import com.ottention.banana.entity.User;
+import com.ottention.banana.entity.notification.Notification;
 import com.ottention.banana.entity.notification.NotificationType;
 import com.ottention.banana.repository.UserRepository;
+import com.ottention.banana.repository.notification.NotificationRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
 class NotificationServiceTest {
     @Autowired
     NotificationService notificationService;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    NotificationRepository notificationRepository;
 
     @Test
     void 알림_구독_진행() {
@@ -38,7 +40,40 @@ class NotificationServiceTest {
 
         //when, then
         Assertions.assertDoesNotThrow(() -> notificationService.send(user, "새로운 방명록이 등록되었습니다.", "url", NotificationType.NEW_GUESTBOOK));
+    }
+
+    @Test
+    void 알림_send() {
+        //given
+        User user = User.builder()
+                .email("a")
+                .nickName("a")
+                .build();
+        userRepository.save(user);
+        String content = "test send method";
+        String url = "test/businesscard/";
+        NotificationType type = NotificationType.NEW_GUESTBOOK;
+
+        //when
+        Assertions.assertDoesNotThrow(() -> notificationService.send(user, content, url, type));
+
 
     }
+
+//    @Test
+//    void Notification_생성() {
+//        User user = User.builder()
+//                .email("a")
+//                .nickName("a")
+//                .build();
+//        userRepository.save(user);
+//        String content = "test send method";
+//        String url = "test/businesscard/";
+//        NotificationType type = NotificationType.NEW_GUESTBOOK;
+//
+//        Notification test = notificationService.creatNotification(user, type, content, url);
+//        Long id = notificationRepository.save(test).getId();
+//        System.out.println("id = " + id);
+//    }
 
 }
