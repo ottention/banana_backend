bd7c9d13046059606c2623119b158028d415c0e3
yeonjy <yeonjypaper@gmail.com>
2023-07-17 22:32:54 +0900

    feat: AccessLevel.PROTECTED 추가

--- src/main/java/com/ottention/banana/entity/notification/NotificationContent.java
+++ src/main/java/com/ottention/banana/entity/notification/NotificationContent.java
@@ -2,12 +2,13 @@ package com.ottention.banana.entity.notification;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NotificationContent {
     @NotBlank(message = "notification content must not be blank")
     private String content;
