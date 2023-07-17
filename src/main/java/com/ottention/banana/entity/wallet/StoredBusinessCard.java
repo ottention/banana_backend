3e0805b054d5b4a775e1888ec457a7729055d327
yeonjy <yeonjypaper@gmail.com>
2023-07-17 23:59:21 +0900

    feat: 1:N 연관관계 매핑 컬럼 추가

--- src/main/java/com/ottention/banana/entity/wallet/StoredBusinessCard.java
+++ src/main/java/com/ottention/banana/entity/wallet/StoredBusinessCard.java
@@ -2,12 +2,17 @@ package com.ottention.banana.entity.wallet;
 
 import com.ottention.banana.entity.BaseEntity;
 import com.ottention.banana.entity.BusinessCard;
+import com.ottention.banana.entity.BusinessCardTag;
 import com.ottention.banana.entity.User;
 import com.ottention.banana.entity.wallet.Category;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -35,6 +40,9 @@ public class StoredBusinessCard extends BaseEntity {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
     private User user;  //명함 저장한 사람
+
+    @OneToMany(mappedBy = "note_id", cascade = ALL)
+    private List<Note> notes = new ArrayList<>();
 
     public void modifyIsBookmarked(boolean isBookmarked) {
         this.isBookmarked = isBookmarked;
