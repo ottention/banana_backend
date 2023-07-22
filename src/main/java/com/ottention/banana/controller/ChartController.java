bd18657fcc0092f89cdb4c237aac873d0e4e97ba
CMHyuk <alsgur9710@naver.com>
2023-07-22 17:56:21 +0900

    refactor : 페이징 처리 서비스 -> 컨트롤러로 인한 수정

--- src/main/java/com/ottention/banana/controller/ChartController.java
+++ src/main/java/com/ottention/banana/controller/ChartController.java
@@ -4,8 +4,10 @@ import com.ottention.banana.config.Login;
 import com.ottention.banana.dto.request.LoginUser;
 import com.ottention.banana.dto.response.businesscard.BusinessCardResponse;
 import com.ottention.banana.dto.response.businesscard.TagResponse;
-import com.ottention.banana.service.ChartService;
+import com.ottention.banana.service.chart.ChartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +26,9 @@ public class ChartController {
     }
 
     @GetMapping("/businessCard/topTenBusinessCards")
-    public List<BusinessCardResponse> getTopTensBusinessCards(@RequestParam String name, @Login LoginUser loginUser) {
-        return chartService.getTopTenBusinessCards(name, loginUser.getId());
+    public List<BusinessCardResponse> getTopTensBusinessCards(@RequestParam String name, @Login LoginUser loginUser,
+                                                              @PageableDefault Pageable pageable) {
+        return chartService.getTopTenBusinessCards(name, loginUser.getId(), pageable);
     }
 
 }
