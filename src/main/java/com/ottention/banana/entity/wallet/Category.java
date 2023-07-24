5bd05d1c017bf19c2145f7bb912a3350586629c7
yeonjy <yeonjypaper@gmail.com>
2023-07-24 19:03:04 +0900

    refactor: 사용하지 않는 일대다 연관관계 삭제

--- src/main/java/com/ottention/banana/entity/wallet/Category.java
+++ src/main/java/com/ottention/banana/entity/wallet/Category.java
@@ -29,9 +29,6 @@ public class Category extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "stored_business_card_id", cascade = ALL)
-    private List<StoredBusinessCard> storedBusinessCards = new ArrayList<>();
-
     public Category(String categoryName, User user) {
         this.categoryName = categoryName;
         this.user = user;
